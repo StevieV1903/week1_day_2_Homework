1b47f84d9dba8d1b732319f7fed75aba2a3c55ae
Steve Vance <steve.vance@hotmail.co.uk>
2019-11-26 16:48:46 +0000

    added bear with empty stomach

--- bears.rb
+++ bears.rb
@@ -1,12 +1,16 @@
 class Bear
 
-attr_reader :name
+attr_reader :name, :type
 
-def initialize(name)
+def initialize(name, type)
   @name = name
+  @type = type
+  @bear_stomach = []
 end
 
-
+def fish_amount
+    return @bear_stomach.length
+  end
 
 
 end
--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -2,18 +2,33 @@ require('minitest/autorun')
 require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative('../bear')
+require_relative('../bears')
+require_relative('../river')
 
 
 class BearTest < Minitest::Test
 
   def setup
     @bear = Bear.new("Yogi", "Grizzly")
+
 end
 
 def test_can_get_bear_name
   assert_equal("Yogi", @bear.name)
 end
+
+def test_can_get_bear_type
+  assert_equal("Grizzly", @bear.type)
+end
+
+def test_does_bear_have_empty_stomach
+  assert_equal(0, @bear.fish_amount)
+end
+
+def test_can_bear_take_fish_from_river
+    @bear.pick_up_fish()
+    assert_equal(1, @bear.fish_amount)
+  end
 
 
 end
